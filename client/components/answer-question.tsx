01e717320a83467a346aeae9ac8f3a85aa65de4c
Tom Randolph <thomas.randolph@markforged.com>
2022-05-21 23:27:55 -0400

    user only submits one word

--- client/components/answer-question.tsx
+++ client/components/answer-question.tsx
@@ -1,31 +1,44 @@
-import { FC, FormEventHandler, useState } from "react";
-
+import { FC, FormEventHandler, ChangeEventHandler, useState } from "react";
+import style from "./answer-question.module.css";
 interface Props {
-  onAnswer: (question: string) => Promise<any>
+  onAnswer: (question: string) => Promise<any>;
 }
 
-export const AnswerQuestion: FC<Props> = ({onAnswer}) => {
+export const AnswerQuestion: FC<Props> = ({ onAnswer }) => {
   const [answer, setAnswer] = useState("");
-  const [disabled, setDisabled] = useState(false)
+  const [disabled, setDisabled] = useState(false);
+  const [invalidText, setInvalidText] = useState<string | null>(null);
   const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    setDisabled(true)
+    setDisabled(true);
     await onAnswer(answer);
-    setAnswer('')
-    setDisabled(false)
-
-  }
+    setAnswer("");
+    setDisabled(false);
+  };
+  const onInput: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const value = event.target.value;
+    setAnswer(value);
+    if (value.split(" ").length > 1) {
+      return setInvalidText("Only submit one word");
+    }
+    return setInvalidText(null);
+  };
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} className={style.form}>
       <input
         disabled={disabled}
         type="text"
         name="Answer"
         placeholder="Your Turn"
         value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
+        onChange={onInput}
       />
-      <input type="submit" value="Answer Question" disabled={disabled} />
+      <span className={style.invalid}>{invalidText}</span>
+      <input
+        type="submit"
+        value="Answer Question"
+        disabled={disabled || invalidText != null}
+      />
     </form>
   );
 };
